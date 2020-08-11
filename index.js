72bdc2695458208ab09f3280e6581490123509c5
Antti Kivi <antti.kivi@visiosto.fi>
2020-08-11 16:00:45 +0300

    Add base files of the action

---
+++ index.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Antti Kivi
+// Licensed under the MIT License
+
+const core = require("@actions/core");
+
+async function run() {
+  try {
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
