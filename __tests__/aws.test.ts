988247da6fa3cd84248f5b90aa17bd8390a2bf58
Antti Kivi <antti.kivi@visiosto.fi>
2020-08-13 23:26:22 +0300

    Add helper function for downloading the AWS CLI

---
+++ __tests__/aws.test.ts
@@ -0,0 +1,22 @@
+// Copyright (c) 2020 Antti Kivi
+// Licensed under the MIT License
+
+import {install} from "../src/aws";
+import * as exec from "@actions/exec";
+
+test("install is successful", async () => {
+  const awsLocation = await install();
+  expect(awsLocation).toBe("/usr/local/bin/aws");
+});
+
+test("AWS is installed successfully", async () => {
+  const awsLocation = await install();
+  const returnCode = await exec.exec(awsLocation + " --version");
+  expect(returnCode).toBe(0);
+});
+
+test("AWS can be called from PATH", async () => {
+  await install();
+  const returnCode = await exec.exec("aws --version");
+  expect(returnCode).toBe(0);
+});
