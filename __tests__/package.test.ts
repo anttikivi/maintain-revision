993c3af17ea5e14f3cfb00f355e32c26a2f5d9a0
Antti Kivi <antti.kivi@visiosto.fi>
2020-08-14 03:15:49 +0300

    Fix the formatting

--- __tests__/package.test.ts
+++ __tests__/package.test.ts
@@ -4,6 +4,8 @@
 import {readPackageVersion} from "../src/package";
 
 test("reads package correctly", async () => {
-  const readVersion = await readPackageVersion("../__tests__/package-test.json");
+  const readVersion = await readPackageVersion(
+    "../__tests__/package-test.json"
+  );
   expect(readVersion).toBe("0.1.0-dev");
 });
