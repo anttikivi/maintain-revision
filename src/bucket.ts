ed68b0a1358a7683f77504a736825ac8fe324d1e
Antti Kivi <antti.kivi@visiosto.fi>
2020-08-25 13:08:11 +0300

    Upload the version to S3 and write the version to package.json

--- src/bucket.ts
+++ src/bucket.ts
@@ -75,3 +75,50 @@ export async function readFile(
     });
   });
 }
+
+export async function putFile(
+  bucketName: string,
+  path: string,
+  content: string,
+  accessKeyId?: string,
+  secretAccessKey?: string,
+  region?: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const s3 = new aws.S3({
+      apiVersion: "2006-03-01",
+      accessKeyId:
+        accessKeyId === undefined
+          ? process.env["AWS_ACCESS_KEY_ID"]
+          : accessKeyId,
+      secretAccessKey:
+        secretAccessKey === undefined
+          ? process.env["AWS_SECRET_ACCESS_KEY"]
+          : secretAccessKey,
+      region: region === undefined ? process.env["AWS_DEFAULT_REGION"] : region
+    });
+    const params = {
+      Bucket: bucketName,
+      Key: path,
+      Body: content
+    };
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        core.warning(
+          "The upload of the file " +
+            path +
+            " with the content " +
+            content +
+            " failed: " +
+            err.code
+        );
+        reject();
+      } else {
+        core.debug(
+          "The latest development version was uploaded to bucket " + bucketName
+        );
+        resolve();
+      }
+    });
+  });
+}
--- src/package.ts
+++ src/package.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2020 Antti Kivi
 // Licensed under the MIT License
 
+import * as fs from "fs";
+
 import * as core from "@actions/core";
 
 export async function readVersion(
@@ -14,5 +16,36 @@ export async function readVersion(
       "The version number field read from the package file is " + version
     );
     resolve(version);
+  });
+}
+
+export async function writeVersion(
+  packageVersion: string,
+  newVersion: string,
+  filename: string = "./package.json"
+): Promise<void> {
+  return new Promise(resolve => {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        core.warning(err);
+      } else {
+        const result = data.replace(packageVersion, newVersion);
+
+        fs.writeFile(filename, result, "utf8", err => {
+          if (err) {
+            core.warning(err);
+          }
+        });
+      }
+    });
+    resolve();
+  });
+}
+
+export async function getDefaultPath(version: string): Promise<string> {
+  return new Promise(resolve => {
+    const repository = process.env["GITHUB_REPOSITORY"]?.replace("-", "_");
+    const path = repository + "/" + version + "_version.txt";
+    resolve(path);
   });
 }
