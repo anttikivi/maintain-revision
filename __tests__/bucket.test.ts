ee46492df89e5d2b95663118a2f78210ceae4d1a
Antti Kivi <antti.kivi@visiosto.fi>
2020-08-25 11:14:34 +0300

    Add function for reading file from S3

--- __tests__/bucket.test.ts
+++ __tests__/bucket.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Antti Kivi
 // Licensed under the MIT License
 
-import {fileExists} from "../src/bucket";
+import {fileExists, readFile} from "../src/bucket";
 
 test("file exists", async () => {
   const exists = await fileExists(
@@ -17,4 +17,12 @@ test("file doesn't exist", async () => {
     "anttikivi/maintain_revision/not-found.txt"
   );
   expect(exists).toBe(false);
+});
+
+test("file contents got correctly", async () => {
+  const contents = await readFile(
+    "anthem-workflows",
+    "anttikivi/maintain_revision/test.txt"
+  );
+  expect(contents).toBe("some text here");
 });
--- src/bucket.ts
+++ src/bucket.ts
@@ -28,7 +28,7 @@ export async function fileExists(
       Bucket: bucketName,
       Key: path
     };
-    s3.headObject(params, function (err, metadata) {
+    s3.headObject(params, (err, data) => {
       if (err && err.code === "NotFound") {
         core.debug("File " + path + " doesn't exist in bucket " + bucketName);
         // Return false as the file doesn't exist.
@@ -36,6 +36,41 @@ export async function fileExists(
       } else {
         core.debug("File " + path + " exists in bucket " + bucketName);
         resolve(true);
+      }
+    });
+  });
+}
+
+export async function readFile(
+  bucketName: string,
+  path: string,
+  accessKeyId?: string,
+  secretAccessKey?: string,
+  region?: string
+): Promise<string> {
+  return new Promise(resolve => {
+    const s3 = new aws.S3({
+      apiVersion: "2006-03-01",
+      accessKeyId:
+        accessKeyId === undefined
+          ? process.env["AWS_ACCESS_KEY_ID"]
+          : accessKeyId,
+      secretAccessKey:
+        secretAccessKey === undefined
+          ? process.env["AWS_SECRET_ACCESS_KEY"]
+          : secretAccessKey,
+      region: region === undefined ? process.env["AWS_DEFAULT_REGION"] : region
+    });
+    const params = {
+      Bucket: bucketName,
+      Key: path
+    };
+    s3.getObject(params, (err, data) => {
+      if (err) {
+        // TODO
+        resolve(undefined);
+      } else {
+        resolve(data.Body?.toString());
       }
     });
   });
