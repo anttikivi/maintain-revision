3c96de5ed0e86cda38e3f47716e64ede97374f23
Antti Kivi <antti.kivi@visiosto.fi>
2020-09-11 13:34:15 +0300

    Rename the JSON file tests

--- __tests__/package.test.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2020 Antti Kivi
-// Licensed under the MIT License
-
-import {readVersion} from "../src/package";
-
-test("reads package correctly", async () => {
-  const version = await readVersion("../__tests__/package-test.json");
-  expect(version).toBe("0.1.0-dev");
-});
