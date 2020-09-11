67987681409ba51e4f095adac7a1ebd66b028e29
Antti Kivi <antti.kivi@visiosto.fi>
2020-09-11 13:38:14 +0300

    Change the path of the text file test

--- __tests__/txt-file.test.ts
+++ __tests__/txt-file.test.ts
@@ -4,6 +4,6 @@
 import {readVersion} from "../src/txt-file";
 
 test("reads text file correctly", async () => {
-  const version = await readVersion("../__tests__/file-test.txt");
+  const version = await readVersion("./__tests__/file-test.txt");
   expect(version).toBe("0.2.0-dev");
 });
